d4a0cf7ba167c876638c768619d20e118a3e0303
YC815 <minnecraft.YC815@gmail.com>
2023-07-19 20:04:41 +0800

    open

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ bot = discord.Bot(intents=intents)
 @bot.event
 async def on_ready():
     print(f">>> {bot.user} is ready <<<")
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game("I am NUN not SUS!"))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(os.getenv('GAME')))
 
 # slash-ping
 @bot.command(description="確認機器人在線狀況")
@@ -35,12 +35,12 @@ async def clear(ctx, num: int):
 @bot.event
 async def on_message(message):
     if message.channel.type == discord.ChannelType.private:
-        channel = bot.get_channel(1051043085666242610)
+        channel = bot.get_channel(int(os.getenv('CHANNEL_ID')))
         msg = message.content
         tl = time.localtime(time.time())
         new_time = "%4s.%2s.%2s %2s'%2s" % (
             str(tl.tm_year), str(tl.tm_mon), str(tl.tm_mday), str(tl.tm_hour), str(tl.tm_min))
-        title = "nick-" + str(new_time)
+        title = str(os.getenv('TITLE_PREFIX')) + str(new_time)
         await channel.create_thread(name=title, content=str(msg))
 
 
